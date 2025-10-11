289037da0d628bd023d7c79e387996608f0f182c
catlovergg1234 <yadedorbek@gmail.com>
2025-10-11 18:58:13 +0300

    Update index.js

--- website/index.js
+++ website/index.js
@@ -26,9 +26,9 @@ app.get("/images/robot.jpg", (req, res) => {
 
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
-    next();
 });
 
 app.listen(process.env.PORT || 3000, "0.0.0.0");
+
 
 
